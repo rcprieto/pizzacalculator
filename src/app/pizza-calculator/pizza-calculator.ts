eac4c4846945746e306504cd468c908b1cf7d051
Rodrigo Prieto <rcprieto@gmail.com>
2025-08-01 16:36:12 -0300

    ok

--- src/app/pizza-calculator/pizza-calculator.ts
+++ src/app/pizza-calculator/pizza-calculator.ts
@@ -21,7 +21,12 @@ export class PizzaCalculatorComponent {
   bigaWater = signal<number | null>(null);
   doughFlour = signal<number | null>(null);
   doughWater = signal<number | null>(null);
+  totalMassa = signal<number | null>(null);
+  totalPizzas = signal<number | null>(null);
+  totalPessoas = signal<number | null>(null);
   numberOfPizzasTotal = signal<number | null>(null);
+  tipo = signal<string>('P30');
+  tipoNome = signal<string>('Pizzas 30cm');
 
   calculated = signal<boolean>(false);
 
@@ -31,14 +36,29 @@ export class PizzaCalculatorComponent {
       return;
     }
 
-    const numberOfPizzas = this.numberOfPeople()! * 0.8;
-    const totalDoughWeight = numberOfPizzas * 250;
+    this.tipoNome.set(this.tipo() == 'P30' ? 'Pizzas 30cm' : this.tipo() == 'P40' ? 'Pizzas 40cm' : 'Pessoas');
+
+    let pizzaPorPessoa = 1;
+    let massaPorPizza = 250;
+
+    if (this.tipo() == 'PE30') pizzaPorPessoa = 0.8;
+    else if (this.tipo() == 'PE40') pizzaPorPessoa = 0.5;
+    else if (this.tipo() == 'P40') pizzaPorPessoa = 0.5;
+    else if (this.tipo() == 'P30') pizzaPorPessoa = 0.8;
+
+    if (this.tipo() == 'P40' || this.tipo() == 'PE40') massaPorPizza = 300;
+
+    const numberOfPizzas = this.numberOfPeople()! * pizzaPorPessoa;
+
+    //Total da Massa
+    const totalDoughWeight = parseInt(numberOfPizzas.toFixed(0)) * massaPorPizza;
+
     const hydrationDecimal = this.hydrationPercentage()! / 100;
 
     const totalFlour = totalDoughWeight / (1 + hydrationDecimal);
     const totalWater = totalDoughWeight - totalFlour;
-    const salt = totalFlour * 0.02; // Assuming 2% salt
-    const yeast = totalFlour / 500; // Assuming 0.2% yeast
+    const salt = totalFlour * 0.02; // Assumindo 2% sal
+    const yeast = totalFlour / 500; // Assumindo 0.2% fermento
 
     const bigaFlour = totalFlour / 2;
     const bigaWater = bigaFlour * 0.5;
@@ -46,15 +66,24 @@ export class PizzaCalculatorComponent {
     const doughFlour = totalFlour - bigaFlour;
     const doughWater = totalWater - bigaWater;
 
-    this.totalFlour.set(parseFloat(totalFlour.toFixed(0)));
-    this.totalWater.set(parseFloat(totalWater.toFixed(0)));
-    this.salt.set(parseFloat(salt.toFixed(0)));
-    this.yeast.set(parseFloat(yeast.toFixed(0)));
-    this.bigaFlour.set(parseFloat(bigaFlour.toFixed(0)));
-    this.bigaWater.set(parseFloat(bigaWater.toFixed(0)));
-    this.doughFlour.set(parseFloat(doughFlour.toFixed(0)));
-    this.doughWater.set(parseFloat(doughWater.toFixed(0)));
-    this.numberOfPizzasTotal.set(parseFloat(numberOfPizzas.toFixed(0)));
+    if (this.tipo() == 'PE30' || this.tipo() == 'PE40') this.totalPessoas.set(this.numberOfPeople());
+    else {
+      const totalP = numberOfPizzas / pizzaPorPessoa;
+
+      this.totalPessoas.set(parseFloat(totalP.toFixed(1)));
+    }
+
+    this.totalPizzas.set(parseFloat(numberOfPizzas.toFixed(0)));
+    this.totalFlour.set(parseFloat(totalFlour.toFixed(1)));
+    this.totalWater.set(parseFloat(totalWater.toFixed(1)));
+    this.totalMassa.set(totalDoughWeight);
+    this.salt.set(parseFloat(salt.toFixed(1)));
+    this.yeast.set(parseFloat(yeast.toFixed(1)));
+    this.bigaFlour.set(parseFloat(bigaFlour.toFixed(1)));
+    this.bigaWater.set(parseFloat(bigaWater.toFixed(1)));
+    this.doughFlour.set(parseFloat(doughFlour.toFixed(1)));
+    this.doughWater.set(parseFloat(doughWater.toFixed(1)));
+    this.numberOfPizzasTotal.set(parseFloat(numberOfPizzas.toFixed(1)));
 
     this.calculated.set(true);
     return;
